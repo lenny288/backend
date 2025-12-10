75a0d30b1f5357942c925d00d0525a670beb8702
lenny288 <lennymbuthi1@gmail.com>
2025-12-10 14:48:22 +0300

    Create custom User model with additional attributes
    Added custom User model with additional fields for age, gender, height, and weight.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=10, null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
