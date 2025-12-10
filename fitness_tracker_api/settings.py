f1473a575efaf4083ab62667d996d18796cc5bc6
lenny288 <lennymbuthi1@gmail.com>
2025-12-10 15:24:44 +0300

    Set custom user model for Django authentication

--- fitness_tracker_api/settings.py
+++ fitness_tracker_api/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+AUTH_USER_MODEL = 'users.User'
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
