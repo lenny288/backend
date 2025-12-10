3d945cacdb0f4a9912cc3eb7f908a9887a682d57
lenny288 <lennymbuthi1@gmail.com>
2025-12-10 17:02:33 +0300

    Update serializers.py

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'age', 'gender', 'height', 'weight']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
